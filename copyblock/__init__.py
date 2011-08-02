88abebc4c153a9761cecc2dc3785cc26a7233013
Steve Ivy <steve@wallrazer.com>
2011-08-02 13:54:54 -0700

    updates

---
+++ copyblock/__init__.py
@@ -0,0 +1,4 @@
+VERSION = (0, # pre-final release
+           4, # feature dot-release
+           3  # fiddly bits
+    )
--- setup.py
+++ setup.py
@@ -3,20 +3,24 @@ Manage website copy as a directory of markdown-formatted files. Insert files as 
 Django templates.
 """
 
-from setuptools import setup
+from setuptools import setup, find_packages
+import os
+from copyblock import VERSION
+
+ld = "\n\n".join(("Copyblock came out of a desire of mine to separate the copy for a site I'm working on from the site templates. Things like welcome messages, intro copy for forms, etc. This is copy I'd like to be able to tweak easily over time without having to redeploy the entire site to make it happen. What I wanted was a system kindof like gettext, but without .po files, the weird syntax, and a separete app to generate the right files.",
+      "What I wanted was really simple: a directory of text files, optionally in [Markdown](http://daringfireball.net/projects/markdown), that could be inserted into my app templates with a template tag. That's what Copyblock does."))
 
 setup(
     name='django-copyblock',
-    version='0.4',
+    version=".".join(map(str, VERSION)),
     author='Steve Ivy',
     author_email='steve@wallrazer.com',
     description='Manage website copy as a directory of markdown-formatted files. Insert files as copy into your Django templates.',
     license='MIT License',
-    packages=find_packages(),
-    requires=(),
+    packages=['copyblock'],
     keywords='django copy markdown',
     url='http://github.com/sivy/django-copyblock/',
-    long_description=__doc__,
+    long_description=ld,
     classifiers=[
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
